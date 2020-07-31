61f63ce5f1d8f7d61d6186ccc2dcd18deb76e038
Ankit Bisht <bishtankit371@gmail.com>
2020-07-31 22:04:44 +0530

    app listen remove

--- app.js
+++ app.js
@@ -274,9 +274,9 @@ if(req.isAuthenticated()){
 });
 
 
-var server = app.listen(process.env.PORT || 5000, function(){
+var server = app.listen(process.env.PORT || 5000, function () {
   var port = server.address().port;
-  console.log("express is working on port "+ port);
+  console.log("Express is working on port " + port);
 });
 
 
